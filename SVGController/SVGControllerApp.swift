3f6d6739c2616b342cbc0e6f7ea850740c22b701
orelsgit <o.zilberman@constru.ai>
2022-02-20 14:04:26 +0200

    Initial Commit

---
+++ SVGController/SVGControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SVGControllerApp.swift
+//  SVGController
+//
+//  Created by Orel Zilberman on 20/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SVGControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
